192d162e2a90286626de68d861c2be9df458831c
icaojun <8508355@qq.com>
2016-10-24 18:21:19 +0800

    modify bean third

--- JobComing/src/com/job/dao/JobKindDao.java
+++ JobComing/src/com/job/dao/JobKindDao.java
@@ -1,7 +1,16 @@
 package com.job.dao;
 
+import com.job.bean.JobKind;
+
 public interface JobKindDao extends QueryDao {
-	
+	// 根据一个工作种类对象插入工作种类表
+	public int add(JobKind jk);
+	//通过工作种类Id返回工作种类对象
+	public JobKind getJobKind(int jobKindId);
+	//通过工作种类名称返回工作种类对象
+	public JobKind getJobKind(String jobKindName);
+	// 返回一个工作种类对象
+	public JobKind getJobKind(String hql, Object[] params);
 	
 	
 }
